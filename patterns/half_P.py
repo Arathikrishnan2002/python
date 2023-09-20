078a34701b3f061f085fb83bf9421f46fc3c53d2
R Arathi krishnan <85704406+Arathikrishnan2002@users.noreply.github.com>
2023-09-20 20:17:35 +0530

    Update half_P.py

--- patterns/half_P.py
+++ patterns/half_P.py
@@ -1,8 +1,9 @@
 # half pyrmid pattern using *
-
-
-n =5
-for i in range(0,n-1):
-    for j in range(0,i+1):
-        print("*",end="")
+n=5
+for i in range(1,n+1):
+    
+    for k in range(i):   
+            print("*",end=" ")
+    for j in range(n-i):
+            print(" ",end=" ")
     print()
