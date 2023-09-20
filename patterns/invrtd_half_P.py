2f3f38781cf08ab5cc6618bf0aa7a8fc00aa779f
R Arathi krishnan <85704406+Arathikrishnan2002@users.noreply.github.com>
2023-09-20 20:19:30 +0530

    Update invrtd_half_P.py

--- patterns/invrtd_half_P.py
+++ patterns/invrtd_half_P.py
@@ -1,6 +1,9 @@
 # INVERTED HALF PYRAMID USING *
-n =5
+n=5
 for i in range(n,0,-1):
-    for j in range(0,i):
-        print("*",end="")
-    print("")
+    
+    for k in range(i):   
+            print("*",end=" ")
+    for j in range(n-i):
+            print(" ",end=" ")
+    print()
