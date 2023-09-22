e2a91459f8f6025e889a830866c21f61a8e86e6c
R Arathi krishnan <85704406+Arathikrishnan2002@users.noreply.github.com>
2023-09-22 23:38:33 +0530

    Update sum.py

--- GFGs/basic/sum.py
+++ GFGs/basic/sum.py
@@ -1,8 +1,41 @@
-''''
+'''
 #adding two numbers
 
 num1 = 15
 num2 = 12
 sum = num1+num2
 print("Sum of" ,num1, "and" ,num2, "is",sum)
+
+#adding two numbers with user input
+
+a = input("enter the first number :")
+b = input("Enter the secomnd number:")
+
+sum = float(a)+float(b)
+print("Sum is ",sum)
+
+
+#with function
+
+def add(a,b):
+    return a+b
+num1 = 15
+num2 = 4
+
+sum = add(num1,num2)
+
+print("Sum is",sum)
+
+
+
+#with add method
+a = 15
+b =9
+
+import operator
+
+sum = operator.add(a,b)
+
+print("sum is",sum)
+
 '''
