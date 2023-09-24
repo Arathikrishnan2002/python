b8acc3de0419bebd76195af60c70ba874ef370d9
R Arathi krishnan <85704406+Arathikrishnan2002@users.noreply.github.com>
2023-09-24 08:28:53 +0530

    Update max.py

--- GFGs/basic/max.py
+++ GFGs/basic/max.py
@@ -11,5 +11,40 @@ def max(a,b):
 a=2
 b=3
 print(max(a,b))
-'''
 
+# using max function
+
+a = 9
+b =7
+
+result = max(a,b)
+print(result)
+
+
+# using ternary operator
+
+a = 15
+b =7
+
+print(a if a>=b else b)
+
+# list comprehension
+
+a = 6
+b=21
+
+x = [ a if a>=b else b]
+print(x)
+
+
+# using sort() method
+
+a =96
+b =92
+
+x = [a,b]
+x.sort()
+print(x)
+print(x[-1])
+
+'''
