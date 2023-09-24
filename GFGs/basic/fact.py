066fdeee3ac19941b4d224b2320cabd6735e1a78
R Arathi krishnan <85704406+Arathikrishnan2002@users.noreply.github.com>
2023-09-24 08:43:22 +0530

    Update fact.py

--- GFGs/basic/fact.py
+++ GFGs/basic/fact.py
@@ -1,3 +1,6 @@
+'''
+
+# factorial of a number using iterative approach
 def factorial(n):
     if n<0:
         return 0
@@ -12,3 +15,39 @@ def factorial(n):
         return fact
 num=5
 print("factorial of " ,num,"is ",factorial(num))
+
+
+#using recursive approach / ternary operator
+
+def fact(n):
+    return 1 if(n==0) or (n==1) else n*fact(n-1)
+    
+num =5
+print("factorial of " ,num ,"is",fact(num))
+
+
+# using for loop
+
+def fact(n):
+   
+    fact = 1
+    for i in range(2,n+1):
+        
+        fact = fact*i
+    return fact
+    
+num = 5
+print("factorial is ",fact(num))
+
+
+
+# using built in function
+
+import math
+def fact(n):
+    return math.factorial(n)
+num = 5
+print("factorial is",fact(num))
+'''
+
+
